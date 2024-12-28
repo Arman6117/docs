f8d021ad6784b49ead1ebed8c38244a072922bae
Arman Patel <vanixgaming634@gmail.com>
2024-12-28 16:45:59 +0530

    feat:Enhance DocumentTable with load more functionality and code formatting improvements

--- src/app/(home)/_components/document-table.tsx
+++ src/app/(home)/_components/document-table.tsx
@@ -2,16 +2,24 @@
 
 import { usePaginatedQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
-import { Table, TableRow, TableHeader, TableHead, TableBody, TableCell } from "@/components/ui/table";
+import {
+  Table,
+  TableRow,
+  TableHeader,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "@/components/ui/table";
 import { LoaderIcon } from "lucide-react";
 import DocumentRow from "./document-row";
+import { Button } from "@/components/ui/button";
 
 const DocumentTable = () => {
-  const { loadMore, results:documents, status } = usePaginatedQuery(
-    api.document.get,
-    {},
-    { initialNumItems: 5 }
-  );
+  const {
+    loadMore,
+    results: documents,
+    status,
+  } = usePaginatedQuery(api.document.get, {}, { initialNumItems: 5 });
   return (
     <div className="flex flex-col gap-5 px-16 py-6 mx-auto max-w-screen">
       {documents === undefined ? (
@@ -30,21 +38,34 @@ const DocumentTable = () => {
           </TableHeader>
           {documents.length === 0 ? (
             <TableBody>
-                <TableRow className="hover:bg-transparent border-none">
-                    <TableCell colSpan={4} className=" h-24 text-center  text-muted-foreground">
-                        No documents found
-                    </TableCell>
-                </TableRow>
+              <TableRow className="hover:bg-transparent border-none">
+                <TableCell
+                  colSpan={4}
+                  className=" h-24 text-center  text-muted-foreground"
+                >
+                  No documents found
+                </TableCell>
+              </TableRow>
             </TableBody>
           ) : (
             <TableBody>
-                {documents.map((doc)=> (
-                    <DocumentRow key={doc._id} document={doc}/>
-                ))}
+              {documents.map((doc) => (
+                <DocumentRow key={doc._id} document={doc} />
+              ))}
             </TableBody>
           )}
         </Table>
       )}
+      <div className="flex items-center justify-center">
+        <Button
+          variant={"ghost"}
+          size={"sm"}
+          onClick={() => loadMore(5)}
+          disabled={status !== "CanLoadMore"}
+        >
+          {status != "CanLoadMore" ? "End of results" : "Load More"}
+        </Button>
+      </div>
     </div>
   );
 };
