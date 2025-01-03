c726b9cea7985407ed1dcfecc3c1e690909424a4
Arman Patel <vanixgaming634@gmail.com>
2025-01-03 12:48:03 +0530

    feat: Simplify document page component and add custom error page with retry functionality

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -4,14 +4,7 @@ import Toolbar from "../_components/toolbar";
 import Navbar from "../_components/navbar";
 import Room from "./room";
 
-const page = async ({
-  params,
-}: {
-  params: Promise<{ documentId: string }>;
-}) => {
-  const documentId = (await params).documentId;
-
-  console.log(documentId);
+const page = async () => {
   return (
     <div className="min-h-screen bg-[#fafbfd]">
       <div className="flex flex-col px-4 pt-2 gap-y-2 top-0 left-0 right-0  fixed z-10 bg-[#fafbfd] print:hidden">
