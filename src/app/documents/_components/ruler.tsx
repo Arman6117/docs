c084bdeb9d3f25028bbe9e6442559e61b0c20692
Arman Patel <vanixgaming634@gmail.com>
2025-01-07 12:46:28 +0530

    feat: Update ruler component to remove TODO comments for collaborative margin adjustments

--- src/app/documents/_components/ruler.tsx
+++ src/app/documents/_components/ruler.tsx
@@ -36,7 +36,7 @@ export const Ruler = () => {
         if (isDraggingLeft) {
           const maxLeftPosition = 816 - rightMargin - 100;
           const newLeftPosition = Math.min(rawPosition, maxLeftPosition);
-          setLeftMargin(newLeftPosition); //TODO:Make Collaborative
+          setLeftMargin(newLeftPosition);
         } else if (isDraggingRight) {
           const maxRightPosition = 816 - (leftMargin + 100);
           const newRightPosition = Math.max(816 - rawPosition, 0);
@@ -44,7 +44,7 @@ export const Ruler = () => {
             newRightPosition,
             maxRightPosition
           );
-          setRightMargin(constrainedRightPosition); //TODO:Make Collaborative
+          setRightMargin(constrainedRightPosition); 
         }
       }
     }
