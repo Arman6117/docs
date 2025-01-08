c955d964270ddb110cab663dee9ac322fb69dec8
Arman Patel <vanixgaming634@gmail.com>
2025-01-08 13:08:57 +0530

    feat: Implement debounced input for document ID editing and add loading/error indicators

--- src/app/documents/_components/document-id.tsx
+++ src/app/documents/_components/document-id.tsx
@@ -1,29 +1,62 @@
 "use client";
-import { BsCloudCheck } from "react-icons/bs";
+import { BsCloudCheck, BsCloudSlash } from "react-icons/bs";
 import { Id } from "../../../../convex/_generated/dataModel";
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
+import { useDebounce } from "@/hooks/use-debounce";
+import { toast } from "sonner";
+import { useStatus } from "@liveblocks/react";
+import { LoaderIcon } from "lucide-react";
 
 const DocumentId = ({ title, id }: { title: string; id: Id<"documents"> }) => {
   const [value, setValue] = useState(title);
 
-  const [isError, setIsError] = useState(false);
+
   const [isPending, setIsPending] = useState(false);
   const [isEditing, setIsEditing] = useState(false);
 
+  const status = useStatus();
+
   const inputRef = useRef<HTMLInputElement>(null);
   const mutate = useMutation(api.document.updateById);
+
+  const debounceUpdate = useDebounce((newValue: string) => {
+    if (newValue === title) return;
+
+    setIsPending(true);
+    mutate({ id, newTitle: newValue })
+      .then(() => toast.success("Title updated"))
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false));
+  });
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsPending(true);
+    mutate({ id, newTitle: value })
+      .then(() => {
+        toast.success("Title updated");
+
+        setIsEditing(false);
+      })
+      .catch(() => toast.error("Something went wrong"))
+      .finally(() => setIsPending(false));
+  };
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = e.target.value
+    const newValue = e.target.value;
     setValue(newValue);
 
-    //TODO:Debounced value
-  }
+    debounceUpdate(newValue);
+  };
+
+  const showLoader =
+    isPending || status === "connecting" || status === "reconnecting";
+  const showError = status === "disconnected";
   return (
     <div className="flex gap-2 items-center">
       {isEditing ? (
-        <form className="relative w-fit max-wj">
+        <form className="relative w-fit max-w" onSubmit={handleSubmit}>
           <span className="invisible whitespace-pre px-1.5 text-lg">
             {value || ""}
           </span>
@@ -31,6 +64,7 @@ const DocumentId = ({ title, id }: { title: string; id: Id<"documents"> }) => {
             ref={inputRef}
             value={value}
             onChange={onChange}
+            onBlur={() => setIsEditing(false)}
             className="absolute inset-0 text-lg text-black px-1.5 bg-transparent truncate"
           />
         </form>
@@ -47,7 +81,11 @@ const DocumentId = ({ title, id }: { title: string; id: Id<"documents"> }) => {
           {title}
         </span>
       )}
-      <BsCloudCheck className="tex" />
+      {!showLoader && !showError && <BsCloudCheck className="size-4" />}
+      {showLoader && (
+        <LoaderIcon className="size-4 text-muted-foreground animate-spin" />
+      )}
+      {showError && <BsCloudSlash className="size-4" />}
     </div>
   );
 };
